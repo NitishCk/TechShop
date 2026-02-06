fa9002927eb50542ce80dbf135dbd21d81bcc8ac
NitishCk <cknitish02@gmail.com>
2026-02-06 22:18:00 +0530

    Minor Updates

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -76,7 +76,7 @@ const Footer = () => {
         <div className="text-start">
           <div className="row pt-2 pl-4">
             <div className="col-8">
-              <p>2025 | All Right Reserved &copy; <span className="footer_hover fw-bolder">Hema Kothapalli</span></p>
+              <p>2025 | All Right Reserved &copy; <span className="footer_hover fw-bolder">Nitish ChittoorKrishnamurthy</span></p>
             </div>
             <div className="col-4 text-end">
               <div className="d-flex flex-row justify-content-evenly">
